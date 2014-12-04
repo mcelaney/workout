af588cdb783cb5d4d5ab99f448f828bdb5b06705
Brian E McElaney <brian.e.mcelaney@gmail.com>
2014-12-03 23:12:50 -0500

    Renamed fixture to factory for accuracy

--- spec/feature_helper.rb
+++ spec/feature_helper.rb
@@ -3,7 +3,7 @@ require 'capybara/rspec'
 require 'rack_session_access/capybara'
 
 RSpec.configure do |config|
-  config.include FixtureMethods
+  config.include FactoryMethods
 
   config.before(:each) do
     Capybara.reset!
